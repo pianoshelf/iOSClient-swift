2ae16bb54fe87c43e0d2f23781a2dd25dab4cb98
winston wu <winxton@gmail.com>
2014-11-23 06:56:20 -0500

    initial commit - empty ios single view project

---
+++ Pianoshelf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pianoshelf
+//
+//  Created by Winston Wu on 2014-11-23.
+//  Copyright (c) 2014 pianoshelf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
